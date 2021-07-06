3cc516c34a93a96e693d5f0d49a71cfa36a0d764
unknown <evertonluizcruzs@gmail.com>
2021-07-06 13:43:18 -0300

    Desenvolvimento com listOf

---
+++ collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,25 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val Joao = Funcionario("Joao", salario =  1000.0)
+    val Pedro = Funcionario("Pedro", salario =  4000.0)
+    val Maria = Funcionario("Maria", salario =  6000.0)
+
+    val funcionarios = listOf(Joao, Pedro, Maria)
+
+    funcionarios.forEach{println(it)}
+
+}
+    data class Funcionario(
+        val nome: String,
+        val salario: Double
+
+    ){
+        override fun toString(): String =
+            """
+                Nome: $nome
+                Salario: $salario
+            """.trimIndent()
+        }
+
+
