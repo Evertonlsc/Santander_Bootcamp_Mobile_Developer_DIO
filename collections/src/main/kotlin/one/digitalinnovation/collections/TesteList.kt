cbfefcedf17f43ebc37cc77785b193480321f63e
unknown <evertonluizcruzs@gmail.com>
2021-07-06 13:58:43 -0300

    listOf

--- collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,14 +1,15 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val Joao = Funcionario("Joao", salario =  1000.0)
-    val Pedro = Funcionario("Pedro", salario =  4000.0)
+    val Joao = Funcionario("Joao", salario =  4000.0)
+    val Pedro = Funcionario("Pedro", salario =  1000.0)
     val Maria = Funcionario("Maria", salario =  6000.0)
 
     val funcionarios = listOf(Joao, Pedro, Maria)
 
     funcionarios.forEach{println(it)}
-
+    println("-----------------------")
+    funcionarios.sortedBy { it.salario }.forEach{ println(it)}
 }
     data class Funcionario(
         val nome: String,
