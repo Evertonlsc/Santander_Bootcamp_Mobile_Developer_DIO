6213798dd92d4d5937686e536eab951fdf6b611e
unknown <evertonluizcruzs@gmail.com>
2021-07-06 11:22:48 -0300

    Operações com Arrays count, find e any

--- collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -15,4 +15,16 @@ fun main() {
     val salariosMaiorQue2500 = salarios.filter {it > 2500.0}
     println("____________________")
     salariosMaiorQue2500.forEach{ println(it)}
+
+    println("____________________")
+    println(salarios.count{it in 2500.0..5000.0})
+
+    println("____________________")
+    println(salarios.find{it == 2250.0})
+    println(salarios.find{it == 500.0})
+
+    println("____________________")
+    println(salarios.any{it == 2250.0})
+    println(salarios.any{it == 7500.0})
+
 }
